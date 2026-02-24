16b655400a1c62c1ac420a61745e9a6a6f7f95d0
sudeeptech <94739504+sudeeptech@users.noreply.github.com>
2026-02-24 07:42:46 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,7 @@ import streamlit as st
 # LangChain imports
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
